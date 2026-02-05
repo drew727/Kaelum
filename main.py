3510a282f999605fc6fe8fe3562038c7144eafe6
drew727 <crease367@gmail.com>
2026-02-05 23:09:01 +0000

    Updated

---
+++ main.py
@@ -0,0 +1,36 @@
+
+# main.py
+import os
+
+import discord
+from discord.ext import commands
+from flask import Flask, jsonify, request, Response, stream_with_context, send_from_directory
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET'])
+def home():
+    return "Bot is running!"
+
+
+class Client(commands.Bot):
+    def __init__(self):
+        super().__init__(
+            command_prefix = commands.when_mentioned_or("!"),
+            intents = discord.Intents.all(),
+            help_command = commands.DefaultHelpCommand(dm_help=True)
+        )
+
+    async def setup_hook(self): #overwriting a handler
+        print(f"\033[31mLogged in as {client.user}\033[39m")
+        cogs_folder = f"{os.path.abspath(os.path.dirname(__file__))}/cogs"
+        for filename in os.listdir(cogs_folder):
+            print(filename)
+            if filename.endswith(".py"):
+                await client.load_extension(f"cogs.{filename[:-3]}")
+        await client.tree.sync()
+        print("Loaded cogs")
+
+if __name__ == '__main__':
+    client = Client()
+    client.run("Token here")
