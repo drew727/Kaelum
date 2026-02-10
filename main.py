c880e42db4e161ec7e753db5165534661b6bed7f
drew727 <crease367@gmail.com>
2026-02-10 12:08:50 +0000

    Updated

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import os
 import discord
 from discord.ext import commands
 import threading
-
+from http.server import HTTPServer, BaseHTTPRequestHandler
 
 class Client(commands.Bot):
     def __init__(self):
@@ -22,5 +22,16 @@ class Client(commands.Bot):
                 await client.load_extension(f"cogs.{filename[:-3]}")
         await client.tree.sync()
         print("Loaded cogs")
+class Handler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)
+        self.end_headers()
+        self.wfile.write(b"Bot is running!")
+
+def run_server():
+    port = int(os.environ.get("PORT", 5000))
+    server = HTTPServer(("0.0.0.0", port), Handler)
+    server.serve_forever()
 client = Client()
+threading.Thread(target=run_server, daemon=True).start()
 client.run(os.environ['DISCORD_TOKEN'])
