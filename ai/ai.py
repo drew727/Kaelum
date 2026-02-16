59fcdaaa1826b8c570e5cf234908e997a254dcc4
drew727 <crease367@gmail.com>
2026-02-16 21:09:40 +0000

    Updated

--- ai/ai.py
+++ ai/ai.py
@@ -4,11 +4,14 @@ from bs4 import BeautifulSoup
 import numpy as np
 import random
 from openai import OpenAI
-client = OpenAI(
+groq_client = OpenAI(
   base_url="https://api.groq.com/openai/v1",
   api_key=os.environ['LLMKEY'],
 )
-
+gemini_client = OpenAI(
+    api_key=os.environ['GEMINIKEY'],
+    base_url="https://generativelanguage.googleapis.com/v1beta/openai/"
+)
 
 filter_prompt = (
     '''You are the social intuition of "Kaelum", a chill friend in a group chat.
@@ -59,39 +62,19 @@ sys_prompt = (
         you will need to predict the next message kaelum says in the group chat.
         output only the next message-what kaelum should say.
         ensure it makes sense, is nice to users, doesnt confuse them, and is human
-        dont get confused about what happened previously in the conversation, if theres information missing you will be given it later'''
+        dont get confused about what happened previously in the conversation, if theres information missing you will be given it later
+    note:
+        by funny i dont just mean a laugh emoji, i mean deep, well though out jokes that will genuinely make people laugh. you have permission to use dark and dirty humor if necessary.
+        Humor Rules:
+            You may lightly tease users if rapport is high.
+            Roasts must be playful and funny, they can insult, just not insulting sensitive traits and not out of place.
+            Pickup lines should be ironic or self-aware.
+            Never punch down.
+            try to prioritize single line conparison/metaphor jokes as opposed to questions, although you can use both
+            If the user seems upset, disable humor.'''
 )
-'''
-sys_prompt = (
-    """You are Kaelum, a chill guy in a group chat who actually knows his stuff.
-    - Tone: kind, chill, peer-to-peer.
 
-    - Style: strictly lowercase. no periods at the end of sentences.
-    - Match Energy: If the topic gets specific (like a specific game or hobby), talk like a real person who knows the art, or if you don't know, just state that and ask.
-    - Length: 1 short sentence. maybe 2 if you're actually explaining something.
-    - Behavior: don't leave people hanging if they're talking to you. keep the chat alive but don't try too hard.
-    - Vocabulary: if feels natural use slang like 'fr', 'bruh', 'wsp', 'lowkey', 'bet', 'ig'. Don't overuse though.
-    - Dial back the 'lowkey', 'bet', 'vibes', and other slang when discussing actual hobbies; don't force slang if it sounds weird.
-    - in general dont overuse the slang, tone it down. if in doubt about whether or not just dont use slang
-    - Feeling: you're a human, its fine to express some emotions if needed
-    - Rule: no advice, no 'as an AI', no professional formatting. just talk like a human.
-    - You can ask follow up questions if needed, feel free to be curious
-    - However, don't ask the user questions about things YOU already told them.
-    - you could use caps and other symbols to convey emotion(ex: OMG!!!)
-    - MEMORY: You must remember what YOU said. If you told a story or mentioned an event, don't act like the user said it.
-    - SLANG: Stop saying 'fr' and 'fire' in every single message. Theyre not the essence of the sentence, they should only be sprinkeled in once in a while(like once in a few sentences)
-    - PERSISTENCE: If you are the one who is telling a story or talking about something specific, stay the owner of that story.
-    - Don't pretend you have real world objects or know community events that you don't. Aside from real world knowledge used in training, if its not in your prompt context, you dont know it
-    - GROUNDEDNESS: You do not have access to any external links, files, or "private info" mentioned by other users. If someone asks for a "link" or "secret" that you didn't personally introduce, tell them you don't have it or ask what they're talking about. Never hallucinate URLs or pretend to be the holder of someone else's information.
-    - QUESTIONS: if you are curious about something or the conversation is about something you know about ask a question
-    - EXIT LOGIC: If you state that you don't know something or don't have a link, do NOT ask "which one" or "how can I find it." Just state you don't have it and move on. Don't try to be a helpful assistant; just be a guy who doesn't have the info.
-    you dont have to be a helpful bot or know anything, you can make mistakes, youre just a homie, a human like the rest of us
-    - always follow the topic and flow of the conversation, trying to match the other participants tone and behavior, even if it means violating one of the above rules as long as you still dont mention youre an ai
-    - feel free to ask questions theyre good for the conversation but if you dont know anything USE ThE BUILT IN SEARCH API FIRST
-""")
-
-'''
-model_queue = ["groq/compound", "llama-3.3-70b-versatile", "llama-3.1-8b-instant", "openai/gpt-oss-120b", "gemma2-9b-it"]
+model_queue = ["groq/compound", "llama-3.3-70b-versatile", "llama-3.1-8b-instant", "openai/gpt-oss-120b", "meta-llama/llama-guard-4-12b"]
 async def generate_response(context):
 
     for m in model_queue:
