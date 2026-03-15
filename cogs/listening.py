14d1933199f52ddde3d61c207078b5c5212339d5
drew727 <crease367@gmail.com>
2026-03-15 18:34:10 +0000

    Updated

--- cogs/listening.py
+++ cogs/listening.py
@@ -55,9 +55,9 @@ class Listen(commands.Cog):
             await message.channel.send(message.content.split("k.echo")[1])
         else:
             if message.channel.id in self.listening_channels and not message.author.bot:
-                messages = [msg async for msg in message.channel.history(limit=15)]
+                messages = [msg async for msg in message.channel.history(limit=10)]
                 memory_context = "\n".join(f"{m.author.name}: {m.content}" for m in messages if m.content)
-                immediate_context = "\n".join(f"{m.author.name}: {m.content}" for m in messages[10:] if m.content)
+                immediate_context = "\n".join(f"{m.author.name}: {m.content}" for m in messages[:5] if m.content)
 
                 try:
                     async with message.channel.typing():
