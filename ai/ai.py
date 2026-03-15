435e34d1311abc70ec16a617a37ffe93431c6dc0
drew727 <crease367@gmail.com>
2026-03-15 17:36:27 +0000

    Updated

--- ai/ai.py
+++ ai/ai.py
@@ -8,7 +8,7 @@ import random
 from openai import AsyncOpenAI
 from google import genai
 from google.genai import types
-from ai.system_instructions import filter_prompt, groq_sysins, gemini_sysins, annoying_sysins, summary_sysins
+from system_instructions import filter_prompt, groq_sysins, gemini_sysins, annoying_sysins, summary_sysins
 #initialize apis
 client = AsyncOpenAI(
   base_url="https://api.groq.com/openai/v1",
