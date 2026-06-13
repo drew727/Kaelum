01d9a700909ee94de60cc3e5ded47f8eb1df3d5c
Samuel Chen <158495861+the-dragonmaster@users.noreply.github.com>
2026-06-13 14:50:03 -0400

    Add console command execution and update permissions

--- cogs/console.py
+++ cogs/console.py
@@ -2,6 +2,7 @@ import discord
 from discord.ext import commands
 import readline
 from cogs.constant import admins
+import os
 
 class Console(commands.Cog):
     def __init__(self, bot):
@@ -14,9 +15,15 @@ class Console(commands.Cog):
             history = []
             for i in range(1, readline.get_current_history_length() + 1):
                 history.append(readline.get_history_item(i))
-            await interaction.response.send_message(" ".join(history))
+            await interaction.response.send_message(" ".join(history), ephemeral=True)
         else:
-            await interaction.response.send_message("no perms ask bot dev zzz")
+            await interaction.response.send_message("no perms ask bot dev zzz", ephemeral=True)
+
+    @discord.app_commands.command(name="execute", description="Execute a command in console")
+    async def execute(self, interaction: discord.Interaction, command: str):
+        if interaction.user.id in self.admins:
+            os.system(command)
+            await interaction.response.send_message(f'Executed "{command}" in terminal', ephemeral=True)
 
 async def setup(client):
     await client.add_cog(Console(client))
