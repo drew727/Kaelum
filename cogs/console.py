6340596968cec6c9da827c46c2439e29e4010f99
Samuel Chen <158495861+the-dragonmaster@users.noreply.github.com>
2026-06-13 14:40:20 -0400

    Restrict console history access to admins only

--- cogs/console.py
+++ cogs/console.py
@@ -1,17 +1,22 @@
 import discord
 from discord.ext import commands
 import readline
+from cogs.constant import admins
 
 class Console(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
+        self.admins = admins
 
     @discord.app_commands.command(name="console", description="Shows console history (I hope)")
     async def spawn(self, interaction: discord.Interaction):
-        history = []
-        for i in range(1, readline.get_current_history_length() + 1):
-            history.append(readline.get_history_item(i))
-        await interaction.response.send_message(" ".join(history))
+        if interaction.user.id in self.admins:
+            history = []
+            for i in range(1, readline.get_current_history_length() + 1):
+                history.append(readline.get_history_item(i))
+            await interaction.response.send_message(" ".join(history))
+        else:
+            await interaction.response.send_message("no perms ask bot dev zzz")
 
 async def setup(client):
     await client.add_cog(Console(client))
