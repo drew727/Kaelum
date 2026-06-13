e407ea3c7467ebd29862e26047b7ebc187588f62
Samuel Chen <158495861+the-dragonmaster@users.noreply.github.com>
2026-06-13 14:35:08 -0400

    Rename Ping to Console and update command to show bot console

--- cogs/console.py
+++ cogs/console.py
@@ -1,13 +1,17 @@
 import discord
 from discord.ext import commands
+import readline
 
-class Ping(commands.Cog):
+class Console(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    @discord.app_commands.command(name="check", description="Makes sure bot is actually running")
+    @discord.app_commands.command(name="console", description="Shows console history (I hope)")
     async def spawn(self, interaction: discord.Interaction):
-        await interaction.response.send_message("I'm here!")
+        history = []
+        for i in range(1, readline.get_current_history_length() + 1):
+            history.append(readline.get_history_item(i))
+        await interaction.response.send_message(" ".join(history))
 
 async def setup(client):
-    await client.add_cog(Ping(client))
+    await client.add_cog(Console(client))
